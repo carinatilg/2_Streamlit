7b8ebe5c61c9fc614e11ead7bcd1a2b4fc28b8b8
carinatilg <tc4640@mci4me.at>
2024-04-15 16:17:42 +0200

    new function in read_data

--- read_data.py
+++ read_data.py
@@ -15,6 +15,21 @@ def get_person_list(person_data):
         name_list.append(person["lastname"] + ", " + person["firstname"])
     return name_list
 
+def find_person_data_by_name():
+    suchstring  = "Huber, Julian"
+
+    # Teilt einen String in und speichert die Ergebnisse in einer Liste
+    two_names = suchstring.split(", ")
+    vorname = two_names[1]
+    nachname = two_names[0]
+
+    person_data = load_person_data()
+
+    # Nun können wir vergleichen bis wir einen Treffer finden
+    for eintrag in person_data:
+        if (eintrag["lastname"] == nachname and eintrag["firstname"] == vorname):
+            print(eintrag)
+    
 
 
 
